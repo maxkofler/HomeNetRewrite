92d40c46aed02792ec0ad7d068310f42da3495e5
Max Kofler <kofler.max.dev@gmail.com>
2021-06-22 18:33:25 +0200

    Removed everything so development can begin

--- homenet/hnnetworking/hnnetworking/hnnetworking.cpp
+++
@@ -1,8 +0,0 @@
-#include "../hnnetworking.h"
-
-HNNetworking::HNNetworking(){
-    FUN();
-    LOGD("Initializing networking submodule");
-
-    this->_server = new QTcpServer();
-}
--- parser/parser.cpp
+++
@@ -1,5 +0,0 @@
-#include "parser.h"
-
-Parser::Parser(){
-    FUN();
-}
