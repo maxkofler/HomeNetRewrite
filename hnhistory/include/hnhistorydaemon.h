847748e29136c70479177f0c606b4d614876ba67
Max Kofler <kofler.max.dev@gmail.com>
2021-11-23 14:34:57 +0100

    Added QThread and HNHistoryDaemon

---
+++ hnhistory/include/hnhistorydaemon.h
@@ -0,0 +1,15 @@
+#ifndef __HNHISTORYDAEMON_H__
+#define __HNHISTORYDAEMON_H__
+
+#include <QThread>
+
+/**
+ * @brief This class implements a daemon for managing history access
+ */
+class HNHistoryDaemon : public QThread{
+
+
+
+};
+
+#endif
---
+++ hnhistory/src/hnhistorydaemon/hnhistorydaemon.cpp
@@ -0,0 +1,2 @@
+#include "hnhistorydaemon.h"
+
